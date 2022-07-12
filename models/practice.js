99e7a4cc01dff7212d3fa4fee6bbe389d7cfcdcb
Alys <alice.g.cooper@gmail.com>
2022-07-12 11:42:42 -0400

    Set up server.js and created skeleton files for models

---
+++ models/practice.js
@@ -0,0 +1,42 @@
+// using an already connected mongoose, not a fresh one from node modules
+const mongoose = require('./connection');
+const commentSchema = require('./comment')
+
+// need to get Schema + model
+// deconstructure syntax - need an object to deconstruct
+// inside of Mongoose, I want the keys that are named Schema and model
+const { Schema, model } = mongoose;
+
+
+// for sub doc relationships, we set up the relationship on the RECEIVING end (ie. fruit in this case)
+const practiceSchema = new Schema({
+    date: Date,
+    // TODO: will have to use Date.now()?
+    owner: {
+        type: Schema.Types.ObjectId,        
+        // a single User ._id field
+        ref: 'User', 
+        // refers to const User = model('User', userSchema)
+        // the string of 'User' is how we reference a model
+    },
+    entries: [entrySchema], //a practice can have many entries
+}, {
+    timestamps: true,
+})
+
+// need to make a model
+// this collection will be called fruits
+const Practice = model('Practice', practiceSchema)
+
+module.exports = Practice
+
+// started my server
+    // .env with port number
+    // npm start
+// connection.js
+    // mongoose to hook up
+// mongoDB
+    // db_uri
+// fruits.js
+    // using mongoose to create a schema
+    // use that schema to create a model
---
+++ server.js
@@ -0,0 +1,65 @@
+/////////////////////////////////
+// import dependencies
+/////////////////////////////////
+// this allows us to load our env variables
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const practiceRoutes = require('./controller/practice_routes')
+const userRoutes = require('./controller/user_routes')
+const entryRoutes = require('./controller/entry_routes')
+
+////////////////////////////////////////////
+// Create our express application object
+////////////////////////////////////////////
+const app = require('liquid-express-views')(express())
+
+////////////////////////////////////////////
+// Middleware
+////////////////////////////////////////////
+// this is for request logging
+app.use(morgan('tiny'))
+app.use(methodOverride('_method'))
+// parses urlencoded request bodies
+app.use(express.urlencoded({ extended: false }))
+// to serve files from public statically
+app.use(express.static('public'))
+// bring in our session middleware
+// bring in our session middleware
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+// here's the middleware that sets up our sessions
+app.use(
+	session({
+		secret: process.env.SECRET,
+		store: MongoStore.create({
+			mongoUrl: process.env.DATABASE_URI
+		}),
+		saveUninitialized: true,
+		resave: false
+	})
+)
+
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.use('/practices', practiceRoutes)
+app.use('/users', userRoutes)
+app.use('/entries', entryRoutes)
+
+// localhost:3000/
+app.get('/', (req, res) => {
+	res.send('your server is running, better go catch it')
+	//res.redirect('/fruits')
+})
+
+////////////////////////////////////////////
+// Server Listener
+////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log(`app is listening on port: ${PORT}`)
+})
